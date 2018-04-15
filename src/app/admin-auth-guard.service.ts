9c4406c555cc0e2d73295264ce6b7ecf4d2d2124
John Morris <johnmorris4@bellsouth.net>
2018-04-15 08:23:15 -0400

    Added the Admin and Authorization sections

---
+++ src/app/admin-auth-guard.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {CanActivate} from '@angular/router';
+import {AuthService} from './auth.service';
+import {UserService} from './user.service';
+
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class AdminAuthGuard implements CanActivate{
+
+  constructor(private auth: AuthService, private userService: UserService) { }
+
+  canActivate(): Observable<boolean> {
+      return this.auth.appUser$
+        .map(appUser => appUser.isAdmin);
+  }
+
+}
